a39f73067d15a1247381f4b7a4c9401edddf0a74
gsx-s1000f <62490804+gsx-s1000f@users.noreply.github.com>
2024-08-15 19:40:17 +0900

    とりあえずコミット。

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,23 +1,43 @@
 package application;
 	
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Properties;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+
+	/** プロパティファイル */
+	final static String PROP_PATH = "./app/application.properties";
+	final Properties prop = new Properties();
+	
+	Scene scene;
+	
+	final String HIGHT_NAME = "app.hight";
+	final String WIDTH_NAME = "app.width";
+	double DEFAULT_HIGHT = 480;
+	double DEFAULT_WIDTH = 640;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			ImageView iv1 = new ImageView();
-			
-			
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
-			Scene scene = new Scene(root,640,480);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			double hight = Double.parseDouble(prop.getProperty(HIGHT_NAME));
+			double width = Double.parseDouble(prop.getProperty(WIDTH_NAME));
+			this.scene = new Scene(root, width, hight);
+			this.scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
@@ -25,6 +45,41 @@ public class Main extends Application {
 		}
 	}
 	
+	@Override
+	public void init() throws Exception {
+		Path path = Paths.get(PROP_PATH);
+		if(Files.exists(path)) {
+			try (BufferedReader br = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
+				prop.load(br);
+			}
+		}
+		String hight = prop.getProperty(HIGHT_NAME);
+		String width = prop.getProperty(WIDTH_NAME);
+		if (hight == null || hight.trim().equals("")) {
+			this.prop.setProperty(HIGHT_NAME, String.valueOf(DEFAULT_HIGHT));
+		}
+		if (width == null || width.trim().equals("")) {
+			this.prop.setProperty(WIDTH_NAME, String.valueOf(DEFAULT_WIDTH));
+		}
+	}
+	@Override
+	public void stop() throws Exception {
+		double hight = Double.parseDouble(prop.getProperty(HIGHT_NAME));
+		double width = Double.parseDouble(prop.getProperty(WIDTH_NAME));		
+		if(this.scene.getHeight() != hight ||
+				this.scene.getWidth() != width) {
+			this.prop.setProperty(HIGHT_NAME, String.valueOf(this.scene.getHeight()));
+			this.prop.setProperty(WIDTH_NAME, String.valueOf(this.scene.getWidth()));
+			
+			Path path = Paths.get(PROP_PATH);
+			try(BufferedWriter bw = Files.newBufferedWriter(path, StandardCharsets.UTF_8)){
+				LocalDateTime date = LocalDateTime.now();
+				String text = date.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+				this.prop.store(bw,text);
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,5 +1,125 @@
 package application;
 
-public class MainController {
-	
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.BorderPane;
+
+public class MainController implements Initializable {
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private BorderPane pain;
+
+    @FXML
+    private ScrollPane scroll;
+
+    @FXML
+    private Label status;
+
+    Image image;
+    boolean onCtrl = false;
+    int zoom = 100;
+    
+    @FXML
+    void onDragDropped(DragEvent event) {
+		Dragboard board = event.getDragboard();
+		if (board.hasFiles()) {
+			board.getFiles().forEach(file -> {
+				this.status.setText(file.getAbsolutePath());
+				try {
+					this.image = new Image(file.toURI().toURL().toString());
+					this.zoom = 100;
+					this.imageView.setFitHeight(this.image.getHeight());
+					this.imageView.setFitWidth(this.image.getWidth());
+					this.imageView.setImage(this.image);
+					this.imageView.setVisible(true);
+				} catch (MalformedURLException e) {
+					e.printStackTrace();
+				}
+			});
+		}
+    }
+    
+    @FXML
+    void onKeyPressed(KeyEvent event) {
+    	System.out.println("onKeyPressed:" + event.getCode());
+    	switch(event.getCode()) {
+    	case KeyCode.CONTROL:
+    		this.onCtrl = true;
+    		break;
+		default:
+    	}
+    }
+
+    @FXML
+    void onKeyReleased(KeyEvent event) {
+    	System.out.println("onKeyReleased:" + event.getCode());
+    	switch(event.getCode()) {
+    	case KeyCode.CONTROL:
+    		this.onCtrl = false;
+    		break;
+		default:
+    	}
+    }
+
+    @FXML
+    void onKeyTyped(KeyEvent event) {
+    	System.out.println("onKeyTyped:" + event.getCode());
+    	switch(event.getCode()) {
+    	case KeyCode.CONTROL:
+    		this.onCtrl = false;
+    		break;
+		default:
+    	}
+    }
+
+    @FXML
+    void onScroll(ScrollEvent event) {
+    	System.out.println("onScroll");
+    	if(this.onCtrl) {
+    		this.zoom = this.zoom - (int)(event.getDeltaY() / 40 * 5);
+    		if(this.zoom < 10) this.zoom = 10;
+    		System.out.println(this.zoom);
+			this.imageView.setFitHeight(this.image.getHeight() * ((double)this.zoom / 100));
+			this.imageView.setFitWidth(this.image.getWidth() * ((double)this.zoom / 100));
+    	}
+    }
+
+    @FXML
+    void onScrollFinished(ScrollEvent event) {
+    	System.out.println("onScrollFinished");
+    }
+
+    @FXML
+    void onScrollStarted(ScrollEvent event) {
+    	System.out.println("onScrollStarted");
+    }
+
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		this.pain.setOnDragOver(event -> {
+			Dragboard board = event.getDragboard();
+			if( board.hasFiles() ) {
+				event.acceptTransferModes(TransferMode.ANY);
+			}
+		});
+	}
 }
